2243da85fc2319781830ebc6427c4056f5883366
Harold Beckerman <halbeckerman@gmail.com>
2018-09-27 23:21:15 -0500

    added sort-by

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
-
+import CreateContact from './CreateContact'
 
   class App extends Component {
     state={
+      screen : 'list', // list, create``
       contacts : []
     }
     componentDidMount() {
@@ -12,18 +13,29 @@ import * as ContactsAPI from './utils/ContactsAPI'
         this.setState({contacts})
       })
     }
+
     removeContact = (contact) => {
       this.setState((state) => ({
-          contacts: state.contacts.filter((c) => c.id !== contact.id)
+          contacts: state.contacts.filter(
+            (c) => c.id !== contact.id)
       }))
 
       ContactsAPI.remove(contact)
     }
     render() {
       return (
-      <div>
+      <div className='app'>
+        {this.state.screen === 'list' && (
         <ListContacts onDeleteContact={this.removeContact}
-          contacts={this.state.contacts}/>
+          contacts={this.state.contacts}
+          onNavigate={() => {
+            this.setState({ screen : 'create'})
+          }}
+        />
+      )}
+      {this.state.screen === 'create' && (
+        <CreateContact/>
+      )}
       </div>
     )
     }
